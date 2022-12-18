caf16ff7029ea564de2dd80d136a76103171d177
Cameron Goddard <camerongoddard@me.com>
2022-12-17 23:31:05 -0500

    Initial Commit

---
+++ DiningBar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DiningBar
+//
+//  Created by Cameron Goddard on 12/17/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
