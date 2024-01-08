981cd0f6ba1f6f7aed9f73c012a9921d5195a67b
Cameron Goddard <camerongoddard@me.com>
2024-01-08 13:43:44 -0700

    change separator cell height

--- Big Red Dining/List View/ListViewController.swift
+++ Big Red Dining/List View/ListViewController.swift
@@ -110,6 +110,9 @@ extension ListViewController: NSTableViewDataSource {
     }
 
     func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
+        if row == currentDiningHalls.count {
+            return 20
+        }
         return 40
     }
 
